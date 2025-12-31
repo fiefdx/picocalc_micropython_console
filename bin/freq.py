78737d6a2ba5013daa5fdd68f6dd7bb7ac510b0b
fiefdx <fiefdx@gmail.com>
2025-12-30 21:20:18 -0700

    add freq command

---
+++ bin/freq.py
@@ -0,0 +1,15 @@
+import machine
+
+coroutine = False
+
+
+def main(*args, **kwargs):
+    result = "path invalid"
+    if len(args) > 0:
+        if args[0].isdigit():
+        	f = int(args[0]) * 1000000
+        	machine.freq(f, f)
+        	result = "CPU speed set to %.2f Mhz" % (machine.freq() / 1000000)
+    else:
+    	result = "CPU speed is %.2f Mhz" % (machine.freq() / 1000000)
+    return result
