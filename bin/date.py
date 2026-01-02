7ce64973c835a53af68fb4f4bc5c4c97da5e7c78
fiefdx <fiefdx@163.com>
2026-01-02 12:10:13 -0700

    update date command

--- bin/date.py
+++ bin/date.py
@@ -2,8 +2,7 @@ import sys
 from machine import Pin, I2C
 
 from scheduler import Condition, Message
-from common import exists, path_join
-from ds3231 import ds3231
+from common import exists, path_join, Time
 
 coroutine = True
 
@@ -13,26 +12,23 @@ def main(*args, **kwargs):
     args = kwargs["args"]
     shell_id = kwargs["shell_id"]
     try:
-        i2c = I2C(1, scl=Pin(27), sda=Pin(26), freq=100000)
-        ups = ds3231(i2c)
         if len(args) > 0:
             if args[0] == "-s":
-                if ups.sync_time():
+                if Time.sync():
                     yield Condition.get().load(sleep = 0, send_msgs = [
-                        Message.get().load({"output": ups.read_time()}, receiver = shell_id)
+                        Message.get().load({"output": Time.now()}, receiver = shell_id)
                     ])
                 else:
                     yield Condition.get().load(sleep = 0, send_msgs = [
                         Message.get().load({"output": "sync failed"}, receiver = shell_id)
                     ])
             else:
-                ups.set_time(" ".join(args))
                 yield Condition.get().load(sleep = 0, send_msgs = [
-                    Message.get().load({"output": ups.read_time()}, receiver = shell_id)
+                    Message.get().load({"output": Time.now()}, receiver = shell_id)
                 ])
         else:
             yield Condition.get().load(sleep = 0, send_msgs = [
-                Message.get().load({"output": ups.read_time()}, receiver = shell_id)
+                Message.get().load({"output": Time.now()}, receiver = shell_id)
             ])
     except Exception as e:
         yield Condition.get().load(sleep = 0, send_msgs = [
