3d452afb7d17d8c6352a4d3d40fdad1be68ce5ea
fiefdx <fiefdx@gmail.com>
2026-01-16 22:25:12 -0700

    df command bug fix

--- bin/df.py
+++ bin/df.py
@@ -3,6 +3,7 @@ import sys
 # import micropython
 
 from scheduler import Condition, Message
+from common import exists
 
 coroutine = True
 
@@ -20,13 +21,13 @@ def main(*args, **kwargs):
         used = size - free
         result += "\n{disk: <8}{total: >10}{free: >10}{used: >10}".format(
             disk = "/", total = "%7.2fK" % (size / 1024), free = "%7.2fK" % (free / 1024), used = "%7.2fK" % (used / 1024))
-
-        stat = os.statvfs("/sd")
-        size = stat[1] * stat[2]
-        free = stat[0] * stat[3]
-        used = size - free
-        result += "\n{disk: <8}{total: >10}{free: >10}{used: >10}".format(
-            disk = "/sd", total = "%8.2fM" % (size / 1024 / 1024), free = "%8.2fM" % (free / 1024 / 1024), used = "%8.2fM" % (used / 1024 / 1024))
+        if exists("/sd"):
+            stat = os.statvfs("/sd")
+            size = stat[1] * stat[2]
+            free = stat[0] * stat[3]
+            used = size - free
+            result += "\n{disk: <8}{total: >10}{free: >10}{used: >10}".format(
+                disk = "/sd", total = "%8.2fM" % (size / 1024 / 1024), free = "%8.2fM" % (free / 1024 / 1024), used = "%8.2fM" % (used / 1024 / 1024))
         yield Condition.get().load(sleep = 0, send_msgs = [
             Message.get().load({"output": result}, receiver = shell_id)
         ])
