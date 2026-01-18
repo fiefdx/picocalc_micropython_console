6c6d880353ee91215ac805472303ff8461c99a42
fiefdx <fiefdx@163.com>
2026-01-18 11:00:29 -0700

    add rwbench command

---
+++ bin/rwbench.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import time
+# import micropython
+
+from scheduler import Condition, Message
+from common import exists, path_join, isfile, isdir, rmtree
+
+coroutine = True
+
+
+def main(*args, **kwargs):
+    args = kwargs["args"]
+    shell_id = kwargs["shell_id"]
+    try:
+        file_size_bytes =  64 * 1024 # 64 KB
+        buffer_size = 512 # 512-byte blocks for writing/reading
+        result = "invalid parameters"
+        if len(args) > 0:
+            target_path = args[0]
+            if exists(target_path) and isfile(target_path):
+                os.remove(target_path)
+            start_time = time.ticks_ms()
+            try:
+                result = ""
+                with open(target_path, 'wb') as f:
+                    data = bytearray(b'A' * buffer_size)
+                    for _ in range(file_size_bytes // buffer_size):
+                        f.write(data)
+                end_time = time.ticks_ms()
+                write_time = time.ticks_diff(end_time, start_time) / 1000
+                write_speed = (file_size_bytes / 1024) / write_time if write_time > 0 else 0
+                result = f"Write completed in {write_time:.2f} seconds."
+                result += f"\nWrite Speed: {write_speed:.2f} KB/s"
+            except Exception as e:
+                result = f"Write error: {e}"
+            start_time = time.ticks_ms()
+            try:
+                with open(target_path, 'rb') as f:
+                    buf = bytearray(buffer_size)
+                    while f.readinto(buf) == buffer_size:
+                        pass
+                end_time = time.ticks_ms()
+                read_time = time.ticks_diff(end_time, start_time) / 1000
+                read_speed = (file_size_bytes / 1024) / read_time if read_time > 0 else 0
+                result += f"\nRead completed in {read_time:.2f} seconds."
+                result += f"\nRead Speed: {read_speed:.2f} KB/s"
+            except Exception as e:
+                result = f"Read error: {e}"
+            if exists(target_path) and isfile(target_path):
+                os.remove(target_path)
+        yield Condition.get().load(sleep = 0, send_msgs = [
+            Message.get().load({"output": result}, receiver = shell_id)
+        ])
+    except Exception as e:
+        yield Condition.get().load(sleep = 0, send_msgs = [
+            Message.get().load({"output": str(sys.print_exception(e))}, receiver = shell_id)
+        ])
