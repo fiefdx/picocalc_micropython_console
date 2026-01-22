5b836b45074485e016f7315fa813ff971d3d6028
fiefdx <fiefdx@gmail.com>
2026-01-21 17:11:23 -0700

    optimize ollama client to reduce memory using

--- ollama.py
+++ ollama.py
@@ -1,10 +1,65 @@
 import os
 import time
 import json
-import urequests as requests
+import request as requests
 
 
-class Chat(object):
+class ContextFile(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        
+    def append(self, message = "{}"):
+        with open(self.file_path, "a+") as f:
+            line = message + ",\n"
+            f.write(line)
+            f.flush()
+        
+    def iter_lines(self):
+        with open(self.file_path, "rb") as f:
+            line = f.readline()
+            while line:
+                next_line = f.readline()
+                if next_line:
+                    yield line
+                else:
+                    yield line[:-2]
+                line = next_line
+                
+    def length(self):
+        result = 0
+        with open(self.file_path, "rb") as f:
+            f.seek(0, 2)
+            result = f.tell() - 2
+        return result
+    
+    def clear(self):
+        with open(self.file_path, "wb") as f:
+            pass
+                
+                
+class ChatData(object):
+    def __init__(self, file_path, model, stream = False):
+        self.head = b'{"model":"%s", "messages":[' % model
+        self.context_file = ContextFile(file_path)
+        self.tail = b'],"stream":%s}' % ('true' if stream else 'false')
+        
+    def length(self):
+        return len(self.head) + len(self.tail) + self.context_file.length()
+        
+    def iter_lines(self):
+        yield self.head
+        for line in self.context_file.iter_lines():
+            yield line
+        yield self.tail
+        
+    def append_message(self, message = "{}"):
+        self.context_file.append(message)
+        
+    def clear(self):
+        self.context_file.clear()
+
+
+class ChatRAM(object):
     def __init__(self, host, port, model, context_length = 10, timeout = 3600, stream = False):
         self.host = host
         self.port = port
@@ -13,6 +68,7 @@ class Chat(object):
         self.stream = stream
         self.context_length = context_length
         self.context = []
+        self.data = ChatData("/chat.data.txt", self.model, self.stream)
         self.headers = {"Content-Type": "application/json"}
         
     def models(self):
@@ -32,7 +88,7 @@ class Chat(object):
         if len(self.context) > self.context_length:
             self.context.pop(0)
         data = {"model": self.model, "messages": self.context, "stream": self.stream}
-        r = requests.post(url, data = json.dumps(data), headers = self.headers, stream = self.stream, timeout = self.timeout)
+        r = requests.post(url, data = json.dumps(data), headers = self.headers, stream = self.stream)
         if r.status_code == 200:
             if self.stream:
                 result = b""
@@ -49,6 +105,115 @@ class Chat(object):
                 return True, response["message"]["content"]
         else:
             return False, r.reason
+        
+        
+class Chat(object):
+    def __init__(self, host, port, model, context_length = 10, timeout = 3600, stream = False, cache_file = "/.chat.cache.txt"):
+        self.host = host
+        self.port = port
+        self.model = model
+        self.timeout = timeout
+        self.stream = stream
+        self.context_length = context_length
+        self.data = ChatData(cache_file, self.model, self.stream)
+        self.headers = {"Content-Type": "application/json"}
+        
+    def change_cache_file(self, cache_file):
+        self.data = ChatData(cache_file, self.model, self.stream)
+        
+    def models(self):
+        url = "http://%s:%s/api/tags" % (self.host, self.port)
+        r = requests.get(url)
+        models = []
+        if r.status_code == 200:
+            mark = '"name":"'
+            mark_i = 0
+            status = "end"
+            name = bytearray()
+            for c in r.iter_chars():
+                if status == "end":
+                    if c == ord(mark[0]):
+                        status = "mark_start"
+                        mark_i = 1
+                elif status == "mark_start":
+                    if c == ord(mark[mark_i]):
+                        mark_i += 1
+                        if mark_i == len(mark):
+                            status = "name_start"
+                    else:
+                        status = "end"
+                elif status == "name_start":
+                    if c == ord('"'):
+                        status = "end"
+                        models.append(name.decode())
+                        name = bytearray()
+                    else:
+                        name.append(c)
+            return True, models
+        else:
+            return False, r.reason
+        
+    def chat(self, message):
+        url = "http://%s:%s/api/chat" % (self.host, self.port)
+        self.data.append_message(b'{"role": "user", "content": "%s"}' % message)
+        r = requests.post(url, data = self.data, headers = self.headers, stream = self.stream)
+        if r.status_code == 200:
+            if self.stream:
+                result = b""
+                line = r.raw.readline()
+                while line:
+                    result += line
+                    line = r.raw.readline()
+                return True, result
+            else:
+                mark_role = 'role":"'
+                mark_role_i = 0
+                mark_content = 'content":"'
+                mark_content_i = 0
+                mark_escape = '\\'
+
+                last_c = ''
+                status = "end"
+                content = bytearray()
+                role = bytearray()
+                for c in r.iter_chars():
+                    if status == "end":
+                        if c == ord(mark_role[0]):
+                            status = "mark_role_start"
+                            mark_role_i = 1
+                        elif c == ord(mark_content[0]):
+                            status = "mark_content_start"
+                            mark_content_i = 1
+                    elif status == "mark_role_start":
+                        if c == ord(mark_role[mark_role_i]):
+                            mark_role_i += 1
+                            if mark_role_i == len(mark_role):
+                                status = "role_start"
+                        else:
+                            status = "end"
+                    elif status == "mark_content_start":
+                        if c == ord(mark_content[mark_content_i]):
+                            mark_content_i += 1
+                            if mark_content_i == len(mark_content):
+                                status = "content_start"
+                        else:
+                            status = "end"
+                    elif status == "role_start":
+                        if c == ord('"') and last_c != ord(mark_escape):
+                            status = "end"
+                        else:
+                            role.append(c)
+                            last_c = c
+                    elif status == "content_start":
+                        if c == ord('"') and last_c != ord(mark_escape):
+                            status = "end"
+                        else:
+                            content.append(c)
+                            last_c = c
+                self.data.append_message(b'{"role": "%s", "content": "%s"}' % (role.decode(), content.decode()))
+                return True, content.decode()
+        else:
+            return False, r.reason
 
     def clear(self):
-        self.context.clear()
+        self.data.clear()
