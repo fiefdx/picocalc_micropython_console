d275990dbaad944409404e2e3eb84c9bcf41e93c
fiefdx <fiefdx@163.com>
2026-01-22 16:54:21 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@ rm ./*.mpy
 rm -rf ./build
 mkdir ./build
 mkdir ./build/bin
+mkdir ./build/lib
+mkdir ./build/lib/mrequests
 
+cd ./lib
+rm -f ./*.mpy
 mpy-cross ./analyzer.py
 mpy-cross ./basic_shell_alone.py
 mpy-cross ./basicparser.py
@@ -26,10 +30,6 @@ mpy-cross ./keyboard.py
 mpy-cross ./lexer.py
 mpy-cross ./listfile_safe.py
 mpy-cross ./listfile.py
-# mpy-cross ./main.basic.py
-# mpy-cross ./main.py
-mpy-cross ./myDMA.py
-mpy-cross ./myPWM.py
 mpy-cross ./ntp.py
 mpy-cross ./ollama.py
 mpy-cross ./program.py
@@ -37,24 +37,27 @@ mpy-cross ./reload.py
 mpy-cross ./request.py
 mpy-cross ./scheduler.py
 mpy-cross ./sdcard.py
-mpy-cross ./settings_esp32s2.py
-mpy-cross ./settings_pico2.py
 mpy-cross ./shell.py
 mpy-cross ./tea.py
-mpy-cross ./test_display.py
-mpy-cross ./test_main.py
 mpy-cross ./uftpd.py
 mpy-cross ./uping.py
 mpy-cross ./wave.py
-# mpy-cross ./wavePlayer.py
 mpy-cross ./wifi.py
 # mpy-cross ./writer_fast.py
 mpy-cross ./writer.py
 mpy-cross ./zipfile.py
+mpy-cross ./mrequests/mrequests.py
+mpy-cross ./mrequests/urlencode.py
+mpy-cross ./mrequests/urlparseqs.py
+mpy-cross ./mrequests/urlunquote.py
 
-mv -f ./*.mpy ./build/
+cp -f ./__init__.py ../build/lib/
+mv -f ./*.mpy ../build/lib/
+cp -f ./mrequests/__init__.py ../build/lib/mrequests/
+mv -f ./mrequests/*.mpy ../build/lib/mrequests/
 
-cd ./bin
+cd ../bin
 rm -f ./*.mpy
-ls | egrep '.py' | awk '{print "mpy-cross ./"$1}' | bash
+ls | egrep '.py' | grep -v '__init__.py' | awk '{print "mpy-cross ./"$1}' | bash
+cp -f ./__init__.py ../build/bin/
 mv -f ./*.mpy ../build/bin/
